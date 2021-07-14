e46a4755bebfef318e9a49bb340c025a4e64390f
arunduttgp <6725558+arunduttgp@users.noreply.github.com>
2021-07-14 06:42:37 +0530

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,93 @@
+const socket = io('/') // Create our socket
+const videoGrid = document.getElementById('video-grid') // Find the Video-Grid element
+
+const myPeer = new Peer() // Creating a peer element which represents the current user
+const myVideo = document.createElement('video') // Create a new video tag to show our video
+myVideo.muted = true // Mute ourselves on our end so there is no feedback loop
+
+// Access the user's video and audio
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream) // Display our video to ourselves
+
+    myPeer.on('call', call => { // When we join someone's room we will receive a call from them
+        call.answer(stream) // Stream them our video/audio
+        const video = document.createElement('video') // Create a video tag for them
+        call.on('stream', userVideoStream => { // When we recieve their stream
+            addVideoStream(video, userVideoStream) // Display their video to ourselves
+        })
+    })
+
+    socket.on('user-connected', userId => { // If a new user connect
+        connectToNewUser(userId, stream) 
+    })
+})
+
+myPeer.on('open', id => { // When we first open the app, have us join a room
+    socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) { // This runs when someone joins our room
+    const call = myPeer.call(userId, stream) // Call the user who just joined
+    // Add their video
+    const video = document.createElement('video') 
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+    // If they leave, remove their video
+    call.on('close', () => {
+        video.remove()
+    })
+}
+
+var userName=prompt("Name please...");
+
+
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream 
+    video.addEventListener('loadedmetadata', () => { // Play the video as it loads
+        video.play()
+    })
+    videoGrid.append(video) // Append video element to videoGrid
+}
+
+
+//------------------------------------------- CHAT SCRIPTS ---------------------------------//
+
+
+const chat = document.querySelector('.chat-form');
+const input = document.querySelector('.chat-input');
+
+chat.addEventListener('submit', event => {
+    event.preventDefault();
+    socket.emit('chat', input.value);
+    input.value = '';
+  });
+
+  
+
+//format message
+  const chatWindow = document.querySelector('.chat-window')
+
+const renderMessage = message => {
+    const div1 = document.createElement('div');
+    div1.classList.add('render-message-username');
+    div1.innerText = userName;
+    chatWindow.appendChild(div1);
+    const div2 = document.createElement('div');
+    div2.classList.add('render-message');
+    div2.innerText = message;
+    chatWindow.appendChild(div2);
+
+    document.querySelector('.chat-window').scrollTop =document.querySelector('.chat-window').scrollHeight;
+}
+
+socket.on('chat', message => {
+  // make sure to modify this
+  renderMessage(message)
+})
+
+
